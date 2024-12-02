c9891e66b38307e6aa79a015babd0a0897406f0e
JESSY798 <jkatt6@unh.newhaven.edu>
2024-12-02 18:00:44 -0500

    Update index.js

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 const http = require("http");
 const fs = require("fs");
 const path = require("path");
+const { MongoClient } = require('mongodb');
 
 const server = http.createServer((req, res) => {
     if (req.url === '/api') {
